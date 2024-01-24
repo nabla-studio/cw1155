cfdfd2aad7f79a9bba7f906c7fd6c6950b4c0cbf
Giorgio Nocera <giorgio.nocera@nabla.studio>
2024-01-24 18:52:06 +0100

    refactor: :recycle: move execute and query, prepare multitest
    Refactoring of the contract file, moving execute and query to external files. Preparing contract for multitest.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 pub mod contract;
 mod error;
+mod execute;
 pub mod helpers;
-pub mod integration_tests;
 pub mod msg;
+#[cfg(test)]
+pub mod multitest;
+mod query;
 pub mod state;
 
 pub use crate::error::ContractError;
