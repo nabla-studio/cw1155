febec51e1d73405ac8aed43db402316dea831360
Giorgio Nocera <giorgio.nocera@nabla.studio>
2024-01-24 18:42:46 +0100

    fix: :bug: fix bug for the schema generation
    Fixing a bug on the pacakge name for the schema generation.

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw1155::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw1155_nabla::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
