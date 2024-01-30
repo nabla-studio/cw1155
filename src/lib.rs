1d298d3cb3770d11ef35b7324c0c46d1dd410f14
Giorgio Nocera <giorgio.nocera@nabla.studio>
2024-01-30 12:16:31 +0100

    refactor: :recycle: refactor lib
    Refactoring the lib file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+// Public modules, accessible from outside the library
 pub mod contract;
-mod error;
-mod execute;
 pub mod helpers;
 pub mod msg;
-#[cfg(any(test, feature = "tests"))]
-pub mod multitest;
-mod query;
 pub mod receiver;
 pub mod state;
 
+// Internal modules, used within the library
+mod error;
+mod execute;
+mod query;
+
+// Public imports for direct use by users of the library
 pub use crate::error::ContractError;
+pub use cw_utils::Expiration;
+
+// Modules specific to testing
+#[cfg(any(test, feature = "tests"))]
+pub mod multitest;
